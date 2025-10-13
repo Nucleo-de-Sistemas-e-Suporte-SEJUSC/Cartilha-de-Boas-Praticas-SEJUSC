2ac0ad5ecab536ceb90f78a86de5a539155a6a17
Miihsoo <163786467+Miihsoo@users.noreply.github.com>
2025-10-13 08:39:29 -0400

    Delete boas-praticas-react/src/components directory

--- boas-praticas-react/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-// Footer.jsx
-export default function Footer() {
-  return (
-    <footer className="app-footer">
-      Desenvolvido pela Gerência de Tecnologia da Informação - SEJUSC
-    </footer>
-  );
-}
